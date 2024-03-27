63f956bf4f20a2856c8c7401ae980959037a05c8
touchtak <159424108+touchtak@users.noreply.github.com>
2024-03-27 06:18:16 +0000

    fin

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,18 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @user = @book.user
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    #redirect_to book_path(book)
   end
 
   def destroy
-    BookComment.find(params[:book_id]).destroy
-    redirect_to book_path
+    BookComment.find(params[:id]).destroy
+    @book = Book.find(params[:book_id])
+    #redirect_to book_path
   end
 
   private
