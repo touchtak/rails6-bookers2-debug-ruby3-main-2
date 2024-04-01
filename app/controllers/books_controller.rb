59925837c2f6469f9e40f72c2e9ad3b3820621b8
touchtak <159424108+touchtak@users.noreply.github.com>
2024-04-01 03:10:22 +0000

    fin

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,9 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    unless ReadCount.find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.read_counts.create(book_id: @book.id)
+    end
     @book_comment = BookComment.new
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :entries, dependent: :destroy
   has_many :messages, dependent: :destroy
+  has_many :read_counts, dependent: :destroy
 
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
