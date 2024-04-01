d57c6814e67c7ab9dda130b8db4b7e6d3557b23e
touchtak <159424108+touchtak@users.noreply.github.com>
2024-04-01 03:03:40 +0000

    8a

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+  has_many :entries, dependent: :destroy
+  has_many :messages, dependent: :destroy
 
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
